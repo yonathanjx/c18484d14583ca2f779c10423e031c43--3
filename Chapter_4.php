27c10f7aeca7a2390cafc4366bf72635a1426346
yonathanjx <yhunetaw@gmail.com>
2023-06-29 07:43:24 +0300

    hey

---
+++ Chapter_4.php
@@ -0,0 +1,16 @@
+
+<?PHP
+//Example 4_1
+
+
+print "---------------Example 4_1----------------";
+print "<br>";
+
+$vegetables = array('apple' => 'red', 'brokelly' => 'green');
+$apple = $vegetable ['apple'];
+echo $apple;
+print "whats up git?";
+print "hello";
+print "hey";
+
+print "<br>";
